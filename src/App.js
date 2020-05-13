bed9fd774e02ef112d6c95219c6608f623ca2deb
Abdou <abdourahamane.ly1@gmail.com>
2020-05-13 12:28:11 +0200

    utilisation de constante

--- src/App.js
+++ src/App.js
@@ -2,22 +2,26 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  const firstName = 'ab';
+  const firstName = 'Thierry';
   const lastName = 'Feuzeu';
   const job  = 'Formateur';
   const ville = 'St Malo';
 
+ 
+
   const getFullName = (firstName, lastName) => `${firstName} ${lastName}`;
+  const placeholder = 'Enter your details';
+
+ 
 
   return (
     <div className="App">
     <h3>Nom complet : {getFullName(firstName, lastName)}</h3>
     <p>Job: {job}</p>
     <p>Ville: {ville}</p>
+    <input placeholder={placeholder} />
     </div>
   );
-
- 
 }
 
 export default App;
