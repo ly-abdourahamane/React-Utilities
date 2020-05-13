3d20d02910aa6ef91b3273dd5eec9fa10aa1f0ec
Abdou <abdourahamane.ly1@gmail.com>
2020-05-13 14:44:50 +0200

    style dans une constante

--- src/App.js
+++ src/App.js
@@ -7,13 +7,17 @@ function App() {
     summary: "Le texte où j'explique donc pourquoi je commence à écrire des textes."
   }
 
+  const styles = {
+    margin: '16px',
+    padding: '16px',
+    boxSizing: 'border-box',
+    borderRadius: '5px',
+    boxShadow: '0 2px 5px #ccc'
+  }
+
   return (
     <div className="App">
-      <div style={{
-        margin: '16px',
-        borderRadius: '5px',
-        boxShadow: '0 2px 5px #ccc'
-      }}>
+      <div style={styles}>
         <h3>{blogEntry.title}</h3>
         <p>{blogEntry.summary}</p>
       </div>
