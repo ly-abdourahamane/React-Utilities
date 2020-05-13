dd1ff62494aad0c4a11ca55bd208dfd2d4c08b2e
Abdou <abdourahamane.ly1@gmail.com>
2020-05-13 12:01:20 +0200

    utilisation des variables

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const fullName = 'Thierry Feuzeu';
+  const job  = 'Formateur';
+  const ville = 'Rennes';
+
+ 
+
   return (
     <div className="App">
-    <h3>Nom complet : Thierry Feuzeu</h3>
-    <p>Job: Developpeur</p>
-    <p>Ville: Antibes</p>
+    <h3>Nom complet : {fullName}</h3>
+    <p>Job: {job}</p>
+    <p>Ville: {ville}</p>
     </div>
   );
 }
