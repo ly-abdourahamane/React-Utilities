1cdec597e1c85e0e0874c3c28ae267910dbb54f4
Abdou <abdourahamane.ly1@gmail.com>
2020-05-13 12:07:05 +0200

    utilisation de template stream

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  const fullName = 'Abdourahamane LY';
-  const job  = 'Developpeur';
-  const ville = 'Antibes';
+  const firstName = 'Thierry';
+  const lastName = 'Feuzeu';
+  const job  = 'Formateur';
+  const ville = 'St Malo';
 
  
 
   return (
     <div className="App">
-    <h3>Nom complet : {fullName}</h3>
+    <h3>Nom complet : {`${firstName} ${lastName}`}</h3>
     <p>Job: {job}</p>
     <p>Ville: {ville}</p>
     </div>
