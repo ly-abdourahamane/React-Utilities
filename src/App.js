9122143335e9319be815cff4967c27a928f8220a
Abdou <abdourahamane.ly1@gmail.com>
2020-05-13 12:18:02 +0200

    utilisation de fonction

--- src/App.js
+++ src/App.js
@@ -2,20 +2,22 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  const firstName = 'Thierry';
+  const firstName = 'ab';
   const lastName = 'Feuzeu';
   const job  = 'Formateur';
   const ville = 'St Malo';
 
- 
+  const getFullName = (firstName, lastName) => `${firstName} ${lastName}`;
 
   return (
     <div className="App">
-    <h3>Nom complet : {`${firstName} ${lastName}`}</h3>
+    <h3>Nom complet : {getFullName(firstName, lastName)}</h3>
     <p>Job: {job}</p>
     <p>Ville: {ville}</p>
     </div>
   );
+
+ 
 }
 
 export default App;
