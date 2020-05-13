c4d6f01fee329b03d22bdc15bda7a16a47f8b62c
Abdou <abdourahamane.ly1@gmail.com>
2020-05-13 20:09:02 +0200

    Utilisation de module pour css

--- src/BlogEntry.js
+++ src/BlogEntry.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import './BlogEntry.css';
+import classes from './BlogEntry.module.css';
 
 function BlogEntry({entry}) {
-  console.log(entry);
+  console.log(entry, classes);
+ // classes.BlogEntry['background-color'] = 'blue';
         return (
-            <div className="BlogEntry">
+            <div className={classes.BlogEntry}>
               <h3>{entry.title}</h3>
               <p>{entry.summary}</p>
               {entry.image && <p>{entry.image}</p>}
