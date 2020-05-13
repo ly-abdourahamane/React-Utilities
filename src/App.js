8162b9d5f5ac3e9ef2ba5c59e09955568bdcdc31
Abdou <abdourahamane.ly1@gmail.com>
2020-05-13 14:36:26 +0200

    Ajout du css

--- src/App.js
+++ src/App.js
@@ -2,27 +2,22 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  const firstName = 'Thierry';
-  const lastName = 'Feuzeu';
-  const job  = 'Formateur';
-  const city = 'St Malo';
-
-  const getFullName = (firstName, lastName) => `${firstName} ${lastName}`;
-
-  const hobbies = [ 'cinema', 'karting' ];
-  const score = 5;
+  const blogEntry = {
+    title: 'Mon premier texte',
+    summary: "Le texte où j'explique donc pourquoi je commence à écrire des textes."
+  }
 
   return (
     <div className="App">
-    <h3>Nom complet : {getFullName(firstName, lastName)}</h3>
-    <p>Job: {job}</p>
-    <p>Ville: {city}</p>
-    <ul>
-      {hobbies.map((hobby) => <li>{hobby}</li>)}
-    </ul>
-    <p>{score > 10 ? "Good" : "Not so good"} score</p>
+      <div style={{
+        margin: '16px',
+        borderRadius: '5px',
+        boxShadow: '0 2px 5px #ccc'
+      }}>
+        <h3>{blogEntry.title}</h3>
+        <p>{blogEntry.summary}</p>
+      </div>
     </div>
   );
 }
-
 export default App;
