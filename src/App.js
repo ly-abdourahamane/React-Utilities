687d140f04dc86b8c0b30efacdb112a05625c9e4
Abdou <abdourahamane.ly1@gmail.com>
2020-05-13 14:54:57 +0200

    Deplacement du style dans le fichier css

--- src/App.js
+++ src/App.js
@@ -7,17 +7,9 @@ function App() {
     summary: "Le texte où j'explique donc pourquoi je commence à écrire des textes."
   }
 
-  const styles = {
-    margin: '16px',
-    padding: '16px',
-    boxSizing: 'border-box',
-    borderRadius: '5px',
-    boxShadow: '0 2px 5px #ccc'
-  }
-
   return (
     <div className="App">
-      <div style={styles}>
+      <div className="BlogEntry">
         <h3>{blogEntry.title}</h3>
         <p>{blogEntry.summary}</p>
       </div>
