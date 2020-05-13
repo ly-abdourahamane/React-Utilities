ec5c4d5252c8634b6dbda9e16b44655fa9dcf00e
Abdou <abdourahamane.ly1@gmail.com>
2020-05-13 13:41:19 +0200

    autre variable

--- src/App.js
+++ src/App.js
@@ -5,21 +5,20 @@ function App() {
   const firstName = 'Thierry';
   const lastName = 'Feuzeu';
   const job  = 'Formateur';
-  const ville = 'St Malo';
-
- 
+  const city = 'St Malo';
 
   const getFullName = (firstName, lastName) => `${firstName} ${lastName}`;
-  const placeholder = 'Enter your details';
 
- 
+  const inputPlaceholder = 'Enter your details';
+  const detailsInput = <input placeholder={inputPlaceholder} />;
+
 
   return (
     <div className="App">
     <h3>Nom complet : {getFullName(firstName, lastName)}</h3>
     <p>Job: {job}</p>
-    <p>Ville: {ville}</p>
-    <input placeholder={placeholder} />
+    <p>Ville: {city}</p>
+    {detailsInput}
     </div>
   );
 }
