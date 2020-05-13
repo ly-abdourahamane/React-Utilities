bc1be79c36ea3a74f785dd96d665f23cd8ad64e9
Abdou <abdourahamane.ly1@gmail.com>
2020-05-13 15:16:56 +0200

    Affichage d'une liste

--- src/App.js
+++ src/App.js
@@ -7,12 +7,37 @@ function App() {
     summary: "Le texte où j'explique donc pourquoi je commence à écrire des textes."
   }
 
+  const blogEntries = [
+    {
+      id: 'id01',
+      title: 'Mon premier texte',
+      summary: "Le texte où j'explique donc pourquoi je commence à écrire des textes."
+    },
+    {
+      id: 'id02',
+      title: 'Mon deuxième texte',
+      summary: "Le texte où j'explique donc pourquoi je commence à écrire des textes."
+    },
+    {
+      id: 'id03',
+      title: 'Mon troisième texte',
+      summary: "Le texte où j'explique donc pourquoi je commence à écrire des textes."
+    }
+  ]
+
+  const blogElements = blogEntries.map((entry) => {
+    console.log(entry);
+    return (
+      <div className="BlogEntry" key={entry.id}>
+        <h3>{entry.title}</h3>
+        <p>{entry.summary}</p>
+      </div>
+    )
+  });
+
   return (
     <div className="App">
-      <div className="BlogEntry">
-        <h3>{blogEntry.title}</h3>
-        <p>{blogEntry.summary}</p>
-      </div>
+      {blogElements}
     </div>
   );
 }
