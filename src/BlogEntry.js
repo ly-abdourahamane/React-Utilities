62f7ceec8cdfaa654bcbed5ff04ca128ec700e7b
Abdou <abdourahamane.ly1@gmail.com>
2020-05-14 12:19:59 +0200

    Tranformation de BlogEntry en class

--- src/BlogEntry.js
+++ src/BlogEntry.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import classes from './BlogEntry.module.css';
 
-function BlogEntry({entry, onClick}) {
-  console.log(entry, classes);
-        return (
-            <div className={classes.BlogEntry} onClick={() => onClick(entry)}>
-              <h3>{entry.title}</h3>
-              <p>{entry.summary}</p>
-              {entry.image && entry.image}
-            </div>
-          )     
-}
+class BlogEntry extends React.Component {
 
+  render()  {
+  console.log(this.props.entry, classes);
+        return (
+            <div className={classes.BlogEntry} onClick={() => this.props.onClick(this.props.entry)}>
+              <h3>{this.props.entry.title}</h3>
+              <p>{this.props.entry.summary}</p>
+              {this.props.entry.image && this.props.entry.image}
+            </div>
+          ) 
+        }    
+}
 export default BlogEntry;
