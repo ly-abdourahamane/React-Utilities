a8385d1528b8105ce3da4815087f0ea5d59d4f11
Abdou <abdourahamane.ly1@gmail.com>
2020-05-14 10:27:06 +0200

    click sur un bouton

--- src/App.js
+++ src/App.js
@@ -5,32 +5,38 @@ import BlogEntry from './BlogEntry';
 function App() {
   const blogEntries = [
     {
-      id: 'id01',
+      id: 'a1',
       title: 'Mon premier texte',
-      summary: "Le texte où j'explique donc pourquoi je commence à écrire des textes.",
-      image: 'image_01'
+      summary: "Le texte où j'explique donc pourquoi je commence à écrire des textes."
     },
     {
-      id: 'id02',
+      id: 'a2',
       title: 'Mon deuxième texte',
       summary: "Le texte où j'explique donc pourquoi je commence à écrire des textes."
     },
     {
-      id: 'id03',
+      id: 'a3',
       title: 'Mon troisième texte',
-      summary: "Le texte où j'explique donc pourquoi je commence à écrire des textes.",
-      image: 'image_03'
+      summary: "Le texte où j'explique donc pourquoi je commence à écrire des textes."
     }
   ]
 
+
+  const onButtonClick = () => {
+    console.log('Clicked on button')
+  }
+
   const blogElements = blogEntries.map((entry) => {
+    console.log(entry)
+
     return (
-      <BlogEntry entry={entry} key={entry.id}/>
+      <BlogEntry key={entry.id} entry={entry}/>
     )
-  });
+  })
 
   return (
     <div className="App">
+      <button onClick={onButtonClick}>Hide Elements</button>
       {blogElements}
     </div>
   );
